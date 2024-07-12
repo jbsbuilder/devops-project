026c2a2cd74d4e2893f48352dd7b87d07edba5cc
jbsbuilder <work.jacobsmith@gmail.com>
2024-07-12 06:58:48 -0600

    err

--- infra/remote_backend_s3.tf
+++ infra/remote_backend_s3.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "dev-proj-1-remote-state-bucket-555555"
     key    = "devops-project-1/terraform.tfstate"
-    region = "us-west-1"
+    region = "us-east-1"
   }
 }
