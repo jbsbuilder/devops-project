ec9c01c2e1c402e7f2d64822d622cc74ae556097
jbsbuilder <work.jacobsmith@gmail.com>
2024-07-14 15:48:32 -0600

    removed bucket

--- infra/remote_backend_s3.tf
+++ infra/remote_backend_s3.tf
@@ -1,7 +1,7 @@
-terraform {
+/*terraform {
   backend "s3" {
     bucket = "dev-proj-1-remote-state-bucket-1"
     key    = "devops-project-1/terraform.tfstate"
     region = "us-east-1"
   }
-}
+}*/
