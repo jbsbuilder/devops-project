3bf007ac32f3b83b4cc105f364e10dab5cda0fd2
jbsbuilder <work.jacobsmith@gmail.com>
2024-07-14 17:05:40 -0600

    changed name of db subgroup id

--- infra/rds/main.tf
+++ infra/rds/main.tf
@@ -9,7 +9,7 @@ variable "mysql_dbname" {}
 # RDS Subnet Group
 resource "aws_db_subnet_group" "dev_proje_1_db_subnet_group" {
   name       = var.db_subnet_group_name
-  subnet_ids = var.subnet_groups # replace with your private subnet IDs
+subnet_ids = module.networking.dev_proj_1_private_subnets# replace with your private subnet IDs
 }
 
 resource "aws_db_instance" "default" {
